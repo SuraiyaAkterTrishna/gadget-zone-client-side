dd1d5455c37cfd092b6a520385b6b04be82272ae
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-05-21 14:18:23 +0600

    Blogs successfully impliment

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -5,28 +5,32 @@ const Blogs = () => {
         <div>
             <div className='bg-primary text-white border rounded p-4 w-75 mx-auto m-5'>
                 <h2>
-                What is the difference between Authorization and Authentication?
+                    What are the difference between Javascript and Node.js?
                 </h2>
                 <p>
-                Authentication is the process of giving assurance in accordance with determining whether an act which is claimed to be correct is actually true. <br />
-                On the other hand, Authorization is the process of giving someone permission to do something or to use a resource.
+                    - Javascript is a programming language that is used for writing scripts on the website. It can only be run in the browsers. It is used in the client-side, so we can say It is used in front-end development. It is the upgraded version of ECMA script that uses Chrome's V8 engine written in C++.
+
+                    - Node.js is a Javascript runtime environment. Javascript can be run outside the browser with the help of Node.js and Node.js is basically used in server side. So we can say that, Node.js is used in server-side development. Nodejs is written in C, C++ and Javascript.
                 </p>
             </div>
             <div className='bg-success text-white border rounded p-4 w-75 mx-auto m-5'>
                 <h2>
-                Why are you using firebase? What other options do you have to implement authentication?
+                    When should we use Node.js and when should we use MongoDB?
                 </h2>
                 <p>
-                Firebase helps us to develop and grow high quality apps. Each feature of it works independently and they work very user friendly together. 
-                Some options to implement authentication: Auth0, OneLogin, Okta, Rippling, SecureAuth Identity Platform, ForgeRock etc.
+                    - Node.js is a Javascript engine that runs our Javascript code. It is used to build servers that can respond to web requests, though it can be used for lots of other types of code too. When we want to write some kind of stand-alone program or server in Javascript, then we can use Node.js for it.
+
+                    - MongoDB is a database engine. Code within some application or server uses MongoDB to save, query or update data in a database. There are many web servers built with Node.js that use MongoDB for storing data. If our application needs the ability to persistently store data, create databases, and also add, query, update or delete data in a way from the database, then we can use MongoDB database.
                 </p>
             </div>
             <div className='bg-danger text-white border rounded p-4 w-75 mx-auto m-5'>
                 <h2>
-                What other services does firebase provide other than authentication?
+                    What are the difference between SQL and No-SQL database?
                 </h2>
                 <p>
-                Firebase provide some other feature than authentication, they are create and implement Realtime Database, Storage, Hosting, Functions, Machine Learning, Firestore Database etc. Firebase simplify testing, triaging and troubleshooting of apps and carefully roll out features and monitor adoption. It also customize our app for different user segments. 
+                    - Structured Query Language (SQL) is a relational database management system. These databases are not suited for hierarchical data storage, these are suited for complex queries. These databases are vertically scalable and follow ACID (Atomicity, Consistency, Isolation and Durability) property.
+
+                    - Non-structured Query Language (No-SQL) is a non-relational or distributed database system. These databases are best suited for hierarchical data storage, these are not suited for complex queries. These databases are horizontally scalable and follow CAP (Consistency, Availability, Partition tolerance) property.
                 </p>
             </div>
         </div>
